1f07d09402db6e28ca5f82f337180a62c37e453e
nikhil-convrseai <115225364+nikhil-convrseai@users.noreply.github.com>
2024-04-07 15:17:03 +0530

    changing and updating playbook

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-apt-get update && apt-get install -y ansible git
+# Install EPEL release for additional packages
+sudo amazon-linux-extras install epel -y
+# Update your system
+sudo yum update -y
+# Install Ansible and Git
+sudo yum install ansible git -y
+# Clone your repository
 git clone https://github.com/odinson-dev/devops_learning.git
+# Navigate to the ansible directory
 cd devops_learning/ansible
+# Run your playbook
 ansible-playbook playbook.yml
